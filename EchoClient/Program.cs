612108f692d7e69346856bf8c3386a613b573ed4
Kasper Duch Jensen <kasp3939@edu.eal.dk>
2017-10-11 12:41:55 +0200

    1

---
+++ EchoClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+
+namespace EchoClient
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Program C = new Program();
+            C.Client();
+        }
+
+        public void Client()
+        {
+            TcpClient client = new TcpClient("localhost", 11000);
+            Console.WriteLine("Client Up");
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+            while (true)
+            {
+                
+                Console.WriteLine("Ready to send");
+                string LineToSend = Console.ReadLine();
+                writer.WriteLine(LineToSend);
+                Console.ReadKey();
+            }
+            client.Close();
+        }
+    }
+}
+
+
+                //TIL AT SENDE BESKEDER
+                //Console.WriteLine("Ready to send");
+                //string LineToSend = Console.ReadLine();
+                //writer.WriteLine(LineToSend);
+                //Console.ReadKey();
